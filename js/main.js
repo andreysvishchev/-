7a39529b1bfb485f1662088fb4b4b31195b9e63c
Andrey Svishchev <andrey.svishchev@yandex.ru>
2021-01-18 12:40:24 +0300

    added menu__btn

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function() {
   $('.carpark__slider').slick({
     arrows: true,
     autoplay: true,
@@ -6,22 +6,25 @@ $(function () {
     responsive: [{
       breakpoint: 1024,
       settings: {
-          arrows : false,
-          dots: true
+        arrows: false,
+        dots: true
       }
-  }, ]
+    }, ]
   });
 
-  
-  $(".menu__list-link, .footer__list-link").on("click", function (e) {
+  $('.menu__btn, .menu a').on('click', function() {
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
+  $(".menu__list-link, .footer__list-link").on("click", function(e) {
 
     e.preventDefault();
     var id = $(this).attr('href'),
-        top = $(id).offset().top;
+      top = $(id).offset().top;
     $('body,html').animate({
-        scrollTop: top
+      scrollTop: top
     }, 1500);
-});
+  });
 
   $(".rate").rateYo({
     starWidth: "15px",
@@ -30,34 +33,33 @@ $(function () {
     readOnly: true
   });
 
-  $(".carpark__filter-item").click(function (e) {
+  $(".carpark__filter-item").click(function(e) {
     $(".carpark__filter-item").removeClass('carpark__filter-item--active');
     $(this).addClass('carpark__filter-item--active');
   });
 
-  $(".btn-one").click(function () {
+  $(".btn-one").click(function() {
     $(".slider-one").addClass("carpark__slider--active");
   });
 
-  $(".btn-two").click(function () {
+  $(".btn-two").click(function() {
     $(".slider-two").addClass("carpark__slider--active");
   });
 
-  $(".btn-three").click(function () {
+  $(".btn-three").click(function() {
     $(".slider-three").addClass("carpark__slider--active");
   });
 
-  $(".btn-one").click(function () {
+  $(".btn-one").click(function() {
     $(".slider-two, .slider-three").removeClass("carpark__slider--active");
   });
 
-  $(".btn-two").click(function () {
+  $(".btn-two").click(function() {
     $(".slider-one, .slider-three").removeClass("carpark__slider--active");
   });
 
-  $(".btn-three").click(function () {
+  $(".btn-three").click(function() {
     $(".slider-one, .slider-two").removeClass("carpark__slider--active");
   });
 
 });
-
